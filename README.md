17b4f9412a69953f03743839af4f1c4a128adc85
Harry Xue <wxue@college.harvard.edu>
2016-01-17 19:55:51 -0500

    Implemented basic functionality

---
+++ README.md
@@ -0,0 +1,2 @@
+A simple chrome extension that changes the default login tab for Harvard PIN login to the HUID option
+for users that don't have HarvardKey
---
+++ changetab.js
@@ -0,0 +1,23 @@
+// Changes the default login tab for Harvard PIN login to the HUID option
+$(document).ready(function () {
+
+	console.log("HUIDdefault extension loaded");
+
+	// Get the divs of the tabs of interest
+	var harvardKeyTab = $("#HarvardKey");
+	var HUIDTab = $("#HUID");
+
+	// Update the classes
+	harvardKeyTab.removeClass("activeTab");
+	HUIDTab.addClass("activeTab");
+
+	// Toggle the aria-selected attribute
+	harvardKeyTab.attr("aria-selected", "false");
+	HUIDTab.attr("aria-selected", "true");
+
+	// Change authentication source type in form
+	var sourceType = $("#compositeAuthenticationSourceType");
+	sourceType.attr("value", "PIN"); 
+});
+
+
